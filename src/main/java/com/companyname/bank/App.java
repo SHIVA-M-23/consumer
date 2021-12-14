676bcfe59bf1c5dfb77f59682a70073d2d86f451
SHIVA-M-23 <94616817+SHIVA-M-23@users.noreply.github.com>
2021-12-14 09:30:19 +0530

    web-hook

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github webhook poll-scm change ");
+        System.out.println( "github webhook webhook change ");
     }
 }
